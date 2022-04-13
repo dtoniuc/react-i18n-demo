6ec6c498181b2f2af27fd67bd25f8ad094f7881e
Daniel Toniuc <dtoniuc@gmail.com>
2022-04-13 17:58:54 +0300

    Reset project to include only the bare minimum

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
 import React from 'react';
 
-import './App.css' 
-
-function App() {
-  return (
-    <div className="container">
+const App = () => (
+    <div className='container'>
         <h1>Hello World</h1>
     </div>
-  );
-}
+)
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from 'react'
+
+import * as ReactDOMClient from 'react-dom/client';
 
 import App from './App';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const root = ReactDOMClient.createRoot(document.getElementById('app-root')!);
+
+root.render(<App/>);
