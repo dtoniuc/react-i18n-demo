75c014d6598717fe293765f9bea9c30ac1e16531
Daniel Toniuc <dtoniuc@gmail.com>
2022-04-13 13:29:55 +0300

    Create react app
    Create react app and clean-up unnecesary code

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+function App() {
+  return (
+    <div className="App">
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+    </div>
+  );
+}
+
+export default App;
