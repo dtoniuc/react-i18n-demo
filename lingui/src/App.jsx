a53439d7de3eea447b9d473412bc0e9fd3e019f6
Daniel Toniuc <dtoniuc@gmail.com>
2022-04-14 11:23:40 +0300

    Add language switcher and dynamic language loading

--- lingui/src/App.jsx
+++ lingui/src/App.jsx
@@ -2,9 +2,18 @@ import React from "react";
 
 import { Trans } from '@lingui/macro'
 
+import { locales, dynamicActivate } from "./i18n";
+
 const App = () => (
     <div>
         <h1><Trans>Hello World</Trans></h1> 
+        <ul>
+        {Object.entries(locales).map(
+            ([localeKey, localeName]) => {
+            return (<li><a key={localeKey} href="#" onClick={() => dynamicActivate(localeKey)}>{localeName}</a></li>)}
+        )}
+        </ul>
+        
     </div>
 );
 
