63548271b2873ec879ac9f8ec35a272ad432bc4d
Daniel Toniuc <dtoniuc@gmail.com>
2022-04-14 11:38:44 +0300

    Format Lingui js code

--- lingui/src/App.jsx
+++ lingui/src/App.jsx
@@ -1,20 +1,30 @@
 import React from "react";
 
-import { Trans } from '@lingui/macro'
+import { Trans } from "@lingui/macro";
 
 import { locales, dynamicActivate } from "./i18n";
 
 const App = () => (
-    <div>
-        <h1><Trans>Hello World</Trans></h1> 
-        <ul>
-        {Object.entries(locales).map(
-            ([localeKey, localeName]) => {
-            return (<li><a key={localeKey} href="#" onClick={() => dynamicActivate(localeKey)}>{localeName}</a></li>)}
-        )}
-        </ul>
-        
-    </div>
+  <div>
+    <h1>
+      <Trans>Hello World</Trans>
+    </h1>
+    <ul>
+      {Object.entries(locales).map(([localeKey, localeName]) => {
+        return (
+          <li>
+            <a
+              key={localeKey}
+              href="#"
+              onClick={() => dynamicActivate(localeKey)}
+            >
+              {localeName}
+            </a>
+          </li>
+        );
+      })}
+    </ul>
+  </div>
 );
 
 export default App;
--- lingui/src/index.jsx
+++ lingui/src/index.jsx
@@ -1,24 +1,24 @@
 import React from "react";
 import ReactDOMClient from "react-dom/client";
 
-import { i18n } from '@lingui/core'
-import { I18nProvider } from '@lingui/react'
-import { defaultLocale, dynamicActivate } from './i18n';
+import { i18n } from "@lingui/core";
+import { I18nProvider } from "@lingui/react";
+import { defaultLocale, dynamicActivate } from "./i18n";
 
 import App from "./App";
 
 const root = ReactDOMClient.createRoot(document.querySelector("#root"));
 
 const I18nApp = () => {
-    React.useEffect(() => {
-        dynamicActivate(defaultLocale)
-    }, []);
+  React.useEffect(() => {
+    dynamicActivate(defaultLocale);
+  }, []);
 
-    return (
-        <I18nProvider i18n={i18n}>
-            <App/>
-        </I18nProvider>
-    )
-}
+  return (
+    <I18nProvider i18n={i18n}>
+      <App />
+    </I18nProvider>
+  );
+};
 
-root.render(<I18nApp/>);
+root.render(<I18nApp />);
